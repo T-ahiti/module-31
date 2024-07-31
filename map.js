444355e5f420ae1cffd8882c7a8403b5bd1420e9
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-31 13:10:10 +0600

    3rc commit

--- map.js
+++ map.js
@@ -1,9 +1,18 @@
 
 const numbers = [32, 3, 3, 2, 5, 4]
 
-const doubled = []
-for(const num of numbers){
-    const double = num * 2
-    doubled.push(double)
+// const doubled = []
+// for(const num of numbers){
+//     const double = num * 2
+//     doubled.push(double)
+// }
+// console.log(doubled)
+
+// alternative way
+
+function doubleIt(num){
+    return num * 2;
 }
-console.log(doubled)
+const result = numbers.map(doubleIt)
+console.log(result)
+
