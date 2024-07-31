14c6bb36a8954cb317c9c07ff24b4e380f3ee2f9
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-31 18:34:14 +0600

    more file added

--- map.js
+++ map.js
@@ -14,5 +14,12 @@ function doubleIt(num){
     return num * 2;
 }
 const result = numbers.map(doubleIt)
-console.log(result)
+// console.log(result)
 
+// map ==> loops through each element of the array and do the operation you passed in the call back function and hold the result from each operation in an array and finally returns you the array.
+
+
+
+// alternative 
+const output = numbers.map(n => n*2)
+console.log(output)
