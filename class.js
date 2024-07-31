196ed91a14e03b59f27185f4cbfaec072f4b2c5b
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-01 00:58:44 +0600

    class file added

---
+++ class.js
@@ -0,0 +1,14 @@
+ 
+  const products = [
+    {id:1, name:'lenovo', price:45389},
+    {id:2, name:'dell', price:43389},
+    {id:3, name:'hp', price:342389},
+    {id:4, name:'mac', price:463489}
+]
+
+// similar
+class Product {
+    constructor (talk){
+      this.name = name;
+    }
+}
