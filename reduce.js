2b45680d01b8ad37363a315c86ba1eea8926a52e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-01 00:32:58 +0600

    second commit

--- reduce.js
+++ reduce.js
@@ -1,1 +1,5 @@
 
+// reduce shobgula element niye kaj kore
+const numbers = [345, 4, 64, 2, 86, 34, 45]
+const total = numbers.reduce( (previous, current) => previous + current ,0)
+console.log(total)
