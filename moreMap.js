32c296dc1d135bfd8dab45bb5426bf2fafb464c7
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-31 21:20:14 +0600

    getting first letter

--- moreMap.js
+++ moreMap.js
@@ -4,3 +4,9 @@ const fiveBonus = numbers.map(num => num + 5)
 console.log(fiveBonus)
 const halves = numbers.map(num => num/2)
 console.log(halves)
+
+const friends = ['tony', 'david', 'richel', 'tom', 'miles']
+const lengths = friends.map(frnd => frnd.length)
+console.log(lengths)
+const firstLetter = friends.map(friend => friend[0])
+console.log(firstLetter)
