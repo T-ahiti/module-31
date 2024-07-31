e0520870556b7fefb52d3dd642e8f57e220a822c
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-01 00:39:22 +0600

    got all names

--- arrayOfObjects.js
+++ arrayOfObjects.js
@@ -1,1 +1,10 @@
 
+const products = [
+    {id:1, name:'lenovo', price:45389},
+    {id:2, name:'dell', price:43389},
+    {id:3, name:'hp', price:342389},
+    {id:4, name:'mac', price:463489}
+]
+
+const names = products.map(p => p.name)
+console.log(names)
