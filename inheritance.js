76ca29cb0830e5fe858b0973834785b28c2fb118
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-01 12:58:07 +0600

    bus and truck added

--- inheritance.js
+++ inheritance.js
@@ -1,2 +1,27 @@
 
+ class Vehicle{
+    constructor(name, price){
+        this.name = name
+        this.price = price
+    }
+    move(){
+        console.log('gari chole na chole na re gari chole na')
+    }
+ }
+
+ class Bus extends Vehicle{
+    constructor(name, price, seat, ticketPrice){
+        super(name,price)
+        this.seat = seat
+        this.ticketPrice= ticketPrice
+    }
+ }
+
+ class truck extends Vehicle{
+    constructor(name, price, load){
+        supers(name, price)
+        this.load = load
+    }
+ }
+
  
